1b6320cd16237b8d6c282bc12c4a093cb306411a
adrimual <adrimual14@gmail.com>
2020-08-20 16:47:43 +0200

    some minor changes

---
+++ go-ride-cli/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Starting</h1>
+        </div>
+    )
+}
+export default App;
