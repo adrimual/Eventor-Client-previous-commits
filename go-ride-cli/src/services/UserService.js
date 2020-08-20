ae056551aa71fd95ae29e45e8553654bc54eb1b5
adrimual <adrimual14@gmail.com>
2020-08-20 17:44:09 +0200

    adding User service

--- go-ride-cli/src/services/UserService.js
+++ go-ride-cli/src/services/UserService.js
@@ -3,13 +3,13 @@ import axios from 'axios'
 export default class UserService {
 
     constructor() {
+
         this.service = axios.create({
-            baseURL: process.env.REACT_APP_API_URL,
+            baseURL: 'http://localhost:4000/api/user',
             withCredentials: true
         })
     }
 
-    editUserProfile = (id, updatedUser) => this.service.put(`user/profile/edit/${id}`, updatedUser)
-    getUserDetails = id => this.service.get(`user/profile/${id}`)
+    editUserProfile = (id, updatedUser) => this.service.post(`edit/${id}`, updatedUser)
 
 }
