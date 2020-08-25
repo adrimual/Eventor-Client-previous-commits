37f420863a6bd25fea7a117e780e533ff5933519
adrimual <adrimual14@gmail.com>
2020-08-25 17:13:59 +0200

    adding profile calendar

--- go-ride-cli/src/components/App.js
+++ go-ride-cli/src/components/App.js
@@ -8,7 +8,7 @@ import ProfilePage from "./profile";
 import PersonEdit from "./profile/personProfile/person-form";
 import EventForm from './event/form'
 import EventList from './event/list'
-
+import Calendar from "./profile-calendar/calendar";
 
 class App extends Component {
     constructor() {
@@ -45,6 +45,7 @@ class App extends Component {
                     <Route exact path="/user/:id/event/edit/:eventId" render={props => this.state.loggedInUser ? <EventForm loggedInUser={this.state.loggedInUser} {...props} personDetails={this.state.loggedInUser.personDetails} /> : <Redirect to='/login' />} />
                     <Route exact path="/events"  render={props => <EventList loggedInUser={this.state.loggedInUser} {...props}  />} />
                     <Route path="/profile/edit/:userId" render={props => this.state.loggedInUser && this.isUserAllowed(this.state.loggedInUser._id, props.match.params.userId) ? <PersonEdit setTheUser={this.setTheUser} loggedInUser={this.state.loggedInUser} {...props} /> : <Redirect to='/login' />}></Route>
+                    <Route path="/profile/:userId/calendar" render={props => <Calendar loggedInUser={this.state.loggedInUser} {...props} /> } />
                     <Route path="/profile/:userId" render={props => this.state.loggedInUser ? <ProfilePage loggedInUser={this.state.loggedInUser} {...props} /> : <h1>{this.state.loggedInUser}</h1>} />
                 </Switch>
 
---
+++ go-ride-cli/src/components/profile-calendar/calendar/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+//services
+import EventService from '../../../services/EventService'
+//import bootstrap
+import { Modal, Container } from "react-bootstrap";
+
+import EventForm from "../../event/form";
+class Calendar extends Component {
+    constructor() {
+        super()
+        this.state = {
+            events: [],
+            showModal: false,
+        }
+        this.eventService = new EventService()
+    }
+    componentDidMount = () => this.updateEvents()
+    updateEvents = () => {
+        const id = this.props.match.params.userId
+        this.eventService.getPersonEvents(id)
+            .then(response => this.setState({ events: response.data }))
+            .catch(err => console.log(err))
+    }
+    handleModal = status => this.setState({ showModal: status })
+
+    handleEventSubmit = () => {
+        this.handleModal(false)
+        this.updateEvents()
+        console.log("updated")
+    }
+    obtainDateInFormat = date => {
+        let newDate = new Date(date)
+        const dd = String(newDate.getDate()).padStart(2, '0')
+        const mm = String(newDate.getMonth() + 1).padStart(2, '0')
+        const yyyy = newDate.getFullYear()
+        newDate = yyyy + '-' + mm + '-' + dd
+        return newDate
+    }
+    render() {
+        const formattedEvents = this.state.events.length > 0 && this.state.events.map(event => { return { title: event.name, date: this.obtainDateInFormat(event.date) } })
+        return (
+            <>
+                <Container>
+                    <FullCalendar
+                        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+                        initialView="dayGridMonth"
+                        selectable={true}
+                        events={formattedEvents}
+                        dateClick={() => this.handleModal(true)}
+                        eventClick={() => alert("touching an event")}
+                        headerToolbar={{ start: "dayGridMonth,timeGridWeek" }}
+                    />
+                </Container>
+                <Modal size="lg" show={this.state.showModal} onHide={() => this.handleModal(false)}>
+                    <Modal.Body>
+                        <EventForm loggedInUser={this.props.loggedInUser} handleModal={this.handleEventSubmit} {...this.props}/>
+                    </Modal.Body>
+                </Modal>
+            </>
+        )
+    }
+}
+
+export default Calendar
