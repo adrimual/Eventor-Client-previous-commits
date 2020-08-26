b1aa322bbf8753cd842604d0304e0fa935f1e4e9
adrimual <adrimual14@gmail.com>
2020-08-27 01:49:00 +0200

    adding row and column to person form

--- go-ride-cli/src/components/pages/profile-page/personProfile/person-form/index.js
+++ go-ride-cli/src/components/pages/profile-page/personProfile/person-form/index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import UserService from '../../../../../services/UserService';
 import FilesService from '../../../../../services/FIlesService';
 //import Bootstrap
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 import SpinnerContainer from "../../../../ui/Spinner"
 class profilePerson extends Component {
     constructor(props) {
@@ -60,45 +60,49 @@ class profilePerson extends Component {
         return (
             <>
                 {!this.props.loggedInUser ? <SpinnerContainer/> :
-                    <Container as='main'>
-                        <Form onSubmit={this.handleFormSubmit}>
-                            <Form.Group>
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control readOnly={true} onChange={this.handleInputChange} value={this.state.username} name="username" type="text" />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control onCharge={this.handleInputChange}
-                                    value={this.setState.state.email}
-                                    name="email"
-                                    type="email">
-                                </Form.Control>
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control onChange={this.handleInputChange} value={this.state.password} name="password" type="password" />
-                                <Form.Text className="text-muted">At least four characters</Form.Text>
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Avatar</Form.Label>
-                                <Form.Control onChange={this.handleFileUpload}  name="avatar" type="file" />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Age</Form.Label>
-                                <Form.Control onChange={this.handleInputChange} value={this.state.age} name="age" type="number" />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.label>Male</Form.label>
-                                <Form.Control onChange={this.handleInputChange} checked={this.state.genre === "Male"} value="Male" name="genre" type="radio" />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Female</Form.Label>
-                                <Form.Control onChange={this.handleInputChange} checked={this.state.genre === "Female"} value="Female" name="genre" type="radio" />
-                            </Form.Group>
-                            {this.state.errorMsg && <p>{this.state.errorMsg}</p>}
-                            <Button variant="dark" type="submit">Submit</Button>
-                        </Form>
-                    </Container>
+                   <Row className='person-form-row'>
+                        <Col  className='person-form-col' md={{span: 6, offset: 3}}>
+                            <h1 className='color-text'>Edit your information</h1>              
+                                <Form onSubmit={this.handleFormSubmit}>
+                                    <Form.Group>
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control readOnly={true} onChange={this.handleInputChange} value={this.state.username} name="username" type="text" />
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control onCharge={this.handleInputChange}
+                                            value={this.setState.state.email}
+                                            name="email"
+                                            type="email">
+                                        </Form.Control>
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control onChange={this.handleInputChange} value={this.state.password} name="password" type="password" />
+                                        <Form.Text className="text-muted">At least four characters</Form.Text>
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Avatar</Form.Label>
+                                        <Form.Control onChange={this.handleFileUpload}  name="avatar" type="file" />
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Age</Form.Label>
+                                        <Form.Control onChange={this.handleInputChange} value={this.state.age} name="age" type="number" />
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.label>Male</Form.label>
+                                        <Form.Control onChange={this.handleInputChange} checked={this.state.genre === "Male"} value="Male" name="genre" type="radio" />
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Female</Form.Label>
+                                        <Form.Control onChange={this.handleInputChange} checked={this.state.genre === "Female"} value="Female" name="genre" type="radio" />
+                                    </Form.Group>
+                                    {this.state.errorMsg && <p>{this.state.errorMsg}</p>}
+                                    <Button variant="dark" type="submit">Submit</Button>
+                                </Form>
+                        </Col>
+                    </Row>
+    
                 };
             </>                   
         );
