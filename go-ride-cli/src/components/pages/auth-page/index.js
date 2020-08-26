4425f91a99376e5e3b294c6bdf3092628f3eacb1
adrimual <adrimual14@gmail.com>
2020-08-26 20:13:23 +0200

    adding some styles

--- go-ride-cli/src/components/pages/auth-page/index.js
+++ go-ride-cli/src/components/pages/auth-page/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './auth-page.css'
-import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
@@ -9,15 +8,15 @@ import AuthForm from './auth-form'
 const AuthPage = props => {
     const isSignup = props.location.pathname === "/signup"
     return (
-        <Container className='container' as="main">
+        <main className='auth-container' as="main">
             <Row>
-                <Col md={{ offset: 3, span: 6 }}>
+                <Col className='auth-form' md={{ offset: 4, span: 4 }}>
                     <h3>{isSignup ? "Sign up" : "Log in"}</h3>
                     <hr></hr>
                     <AuthForm setTheUser={props.setTheUser} {...props} isSignup={isSignup} />
                 </Col>
             </Row>
-        </Container>
+        </main>
     )
 }
 
